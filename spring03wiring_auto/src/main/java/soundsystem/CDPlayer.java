ad510cc7e88e44c1ab66182db8af69db34c73de5
zhangfan <sa517486@mail.ustc.edu.cn>
2020-05-01 00:49:54 +0800

    多参构造函数的@Autowired

--- spring03wiring_auto/src/main/java/soundsystem/CDPlayer.java
+++ spring03wiring_auto/src/main/java/soundsystem/CDPlayer.java
@@ -8,6 +8,8 @@ public class CDPlayer {
 
     private compactDisc cd;
 
+    private Power power;
+
     public CDPlayer() {
         super();
         System.out.println("CDPlayer的无参构造函数");
@@ -15,13 +17,22 @@ public class CDPlayer {
 
     //怎么创建compactDisc和CDPlayer的关联关系呢？答：创建一个有参构造函数！！！并且还要写AutoWired自动装配注解
     //使用alt+insert选择constructor，如下所示
+//    @Autowired
+//    public CDPlayer(compactDisc cd) {
+//        this.cd = cd;
+//        System.out.println("CDPlayer的有参构造函数。。。。");
+//    }
+
+
     @Autowired
-    public CDPlayer(compactDisc cd) {
+    public CDPlayer(compactDisc cd, Power power) {
         this.cd = cd;
-        System.out.println("CDPlayer的有参构造函数。。。。");
+        this.power = power;
+        System.out.println("CDPlayer的多参数构造函数。。。。");
     }
 
     public void play(){
+        power.supply();
         System.out.println("调用了CDPlayer的play方法。。。。");
         cd.play();
     }
---
+++ spring03wiring_auto/src/main/java/soundsystem/Power.java
@@ -0,0 +1,13 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Power {
+    public Power() {
+        super();
+    }
+    public void supply(){
+        System.out.println("电源供电中。。。。");
+    }
+}
