fabc978b75b88ba5a4672981a1478cdc816feb6d
zhangfan <sa517486@mail.ustc.edu.cn>
2020-05-10 16:11:10 +0800

    jdk自带的resource注解，避免歧义性

--- spring04wiring_interface/src/test/java/com/zfedu/demo/service/UserServiceTest.java
+++ spring04wiring_interface/src/test/java/com/zfedu/demo/service/UserServiceTest.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;//不用spring时，可以用jdk自带的注解，
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class UserServiceTest {
 
-    @Autowired
-    @Qualifier("normal")
+//    @Autowired
+//    @Qualifier("normal")
+    @Resource(name = "userServiceNormal")//jdk自带的注解，替代上面两行
     private UserService userService;
 
     @Test
