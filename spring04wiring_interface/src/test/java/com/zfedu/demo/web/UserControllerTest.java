2ea74f31af0f1ba332821b2bc4e1011cd23a9ee7
zhangfan <sa517486@mail.ustc.edu.cn>
2020-05-10 17:08:48 +0800

    通过xml文件实现组件扫描，不仅仅由jdk的Appconfig类啦~~~

--- spring04wiring_interface/src/test/java/com/zfedu/demo/web/UserControllerTest.java
+++ spring04wiring_interface/src/test/java/com/zfedu/demo/web/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.zfedu.demo.web;
 
-import com.zfedu.demo.config.AppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by ZhangFan 2020/5/10
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AppConfig.class)
+//@ContextConfiguration(classes = AppConfig.class)
+@ContextConfiguration ("classpath:applicationContext.xml")
 public class UserControllerTest {
     @Autowired
     private UserController userController;
