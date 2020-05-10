2e46a35b830ee5fd54eb4d6217e7f99d200c3627
zhangfan <sa517486@mail.ustc.edu.cn>
2020-05-10 22:45:15 +0800

    用三种方法解决javaConfig装配的歧义性

--- spring05javaconfig/src/main/java/com/zfedu/demo/config/AppConfig.java
+++ spring05javaconfig/src/main/java/com/zfedu/demo/config/AppConfig.java
@@ -1,11 +1,14 @@
 package com.zfedu.demo.config;
 
 import com.zfedu.demo.dao.UserDao;
+import com.zfedu.demo.dao.impl.UserDaoCache;
 import com.zfedu.demo.dao.impl.UserDaoNormal;
 import com.zfedu.demo.service.UserService;
 import com.zfedu.demo.service.impl.UserServiceNormal;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Created by ZhangFan 2020/5/10
@@ -13,13 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
     @Bean
+//    @Qualifier("normal")
     public UserDao userDaoNormal(){
-        System.out.println("创建UserDao对象的被@bean注解自动调用了");
+        System.out.println("创建UserDaoNormal的bean被查找调用了");
         return new UserDaoNormal();
     }
 
     @Bean
-    public UserServiceNormal userServiceNormal(UserDao userDao){
+//    @Primary  //这是首选bean，解决javaconfig装配歧义性的方法之一
+//    @Qualifier("cache")
+    public UserDao userDaoCache(){
+        System.out.println("创建UserDaoCache的bean被查找调用了");
+        return new UserDaoCache();
+    }
+    @Bean
+    public UserServiceNormal userServiceNormal(@Qualifier("userDaoNormal") UserDao userDao){
         System.out.println("创建UserService对象bean自动调用");
 //        UserDao userDao = userDaoNormal();
         UserServiceNormal userServiceNormal = new UserServiceNormal();
--- spring05javaconfig/src/test/java/com/zfedu/demo/dao/UserDaoTest.java
+++ spring05javaconfig/src/test/java/com/zfedu/demo/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import com.zfedu.demo.config.AppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class UserDaoTest {
 
     @Autowired
+    @Qualifier("userDaoNormal")
     private UserDao userDao;
 
     @Test
