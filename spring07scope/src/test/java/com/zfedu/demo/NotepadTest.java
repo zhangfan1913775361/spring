377d19ecfd7d97985d6e619ccd7fa52863179ad6
zhangfan <sa517486@mail.ustc.edu.cn>
2020-05-14 12:08:24 +0800

    spring中的单例bean

--- spring07scope/src/test/java/com/zfedu/demo/NotepadTest.java
+++ spring07scope/src/test/java/com/zfedu/demo/NotepadTest.java
@@ -14,6 +14,7 @@ public class NotepadTest {
 
     @Test
     public void test1(){
+        //下面这行是用XML配置的加载方式，如果用注解，则不用写
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Notepad notepad1 = (Notepad)context.getBean("notepad");
         Notepad notepad2 = (Notepad)context.getBean("notepad");
--- spring07scope/src/test/java/com/zfedu/demo/NotepadTestAutowired.java
+++ spring07scope/src/test/java/com/zfedu/demo/NotepadTestAutowired.java
@@ -2,6 +2,7 @@ package com.zfedu.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,8 +14,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:applicationContext.xml")//这一行注解，就相当于spring上下文加载了，所以单元测试里什么都不用写
 public class NotepadTestAutowired {
 
+    @Autowired
+    private Notepad notepad1;
+
+    @Autowired
+    private Notepad notepad2;
+
     @Test
     public void test01(){
+//        Notepad notepad1 = (Notepad)context.getBean("notepad");
+//        Notepad notepad2 = (Notepad)context.getBean("notepad");
+        System.out.println(notepad1 == notepad2);//无论注入多少次，你最终获取的都是同一个，所以为true；
 
     }
 }
